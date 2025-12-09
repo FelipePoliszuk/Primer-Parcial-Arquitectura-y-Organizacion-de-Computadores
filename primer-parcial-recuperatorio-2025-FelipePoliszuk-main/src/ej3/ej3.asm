73d2fd6d3b03e7ade3f9635eea00ad08084788e9
FelipePoliszuk <poliszukfelipe@gmail.com>
2025-12-09 12:41:02 -0300

    subo recu mio de 0

---
+++ primer-parcial-recuperatorio-2025-FelipePoliszuk-main/src/ej3/ej3.asm
@@ -0,0 +1,50 @@
+; ------------------------
+; Offsets para los structs
+; Plataforma: x86_64 (LP64)
+; ------------------------
+
+section .data
+
+section .text
+
+; COMPLETAR las definiciones (serán revisadas por ABI enforcer):
+; ------------------------
+; Contenido
+; ------------------------
+CONT_NOMBRE_OFFSET      EQU 0        ; char nombre[64]
+CONT_VALOR_OFFSET       EQU 0       ; uint32_t valor
+CONT_COLOR_OFFSET       EQU 0       ; char color[32]
+CONT_ES_TESORO_OFFSET   EQU 0      ; bool es_tesoro
+CONT_PESO_OFFSET        EQU 0      ; float peso
+CONT_SIZE               EQU 0      ; sizeof(Contenido) (rounded)
+
+; ------------------------
+; Habitacion
+; ------------------------
+HAB_ID_OFFSET          EQU 0         ; uint32_t id
+HAB_VECINOS_OFFSET     EQU 0        ; uint32_t vecinos[ACC_CANT] (4 entradas)
+HAB_CONTENIDO_OFFSET   EQU 0        ; Contenido contenido (aligned to 4)
+HAB_VISITAS_OFFSET     EQU 0       ; uint32_t visitas
+HAB_SIZE               EQU 0       ; sizeof(Habitacion)
+
+; ------------------------
+; Mapa
+; ------------------------
+MAP_HABITACIONES_OFFSET    EQU 0     ; Habitacion *habitaciones  (pointer, 8 bytes)
+MAP_N_HABITACIONES_OFFSET  EQU 0     ; uint64_t n_habitaciones       (8 bytes)
+MAP_ID_ENTRADA_OFFSET      EQU 0    ; uint32_t id_entrada         (4 bytes)
+MAP_SIZE                   EQU 0    ; sizeof(Mapa) (padded to 8)
+
+; ------------------------
+; Recorrido
+; ------------------------
+REC_ACCIONES_OFFSET        EQU 0     ; Accion *acciones  (pointer, 8 bytes)
+REC_CANT_ACCIONES_OFFSET   EQU 0     ; uint64_t cant_acciones (8 bytes)
+REC_SIZE                  EQU 0     ; sizeof(Recorrido)
+
+; Notar que el enum aparece como puntero, entonces no afecta los offsets
+
+global  sumarTesoros
+sumarTesoros
+    ret
+    
