2211485a063e3fc22fd7ea8e62c61b5d32365913
FelipePoliszuk <felipepoliszuk@gmail.com>
2026-09-06 23:29:18 -0300

    Apartado Alineación y Estructuras terminado

--- Guía de ASM 2c2026/src/3 - Alineación y Estructuras/Estructuras.asm
+++ Guía de ASM 2c2026/src/3 - Alineación y Estructuras/Estructuras.asm
@@ -1,20 +1,30 @@
-
+extern cantidad_total_de_elementosC
+extern cantidad_total_de_elementos_packedC
 
 ;########### ESTOS SON LOS OFFSETS Y TAMAÑO DE LOS STRUCTS
 ; Completar las definiciones (serán revisadas por ABI enforcer):
-NODO_OFFSET_NEXT EQU 8
+NODO_OFFSET_NEXT EQU 0
 NODO_OFFSET_CATEGORIA EQU 8
-NODO_OFFSET_ARREGLO EQU 8
-NODO_OFFSET_LONGITUD EQU 8
-NODO_SIZE EQU 8
-PACKED_NODO_OFFSET_NEXT EQU 8
+NODO_OFFSET_ARREGLO EQU 16
+NODO_OFFSET_LONGITUD EQU 24
+; -------
+NODO_SIZE EQU 32
+
+
+PACKED_NODO_OFFSET_NEXT EQU 0
 PACKED_NODO_OFFSET_CATEGORIA EQU 8
-PACKED_NODO_OFFSET_ARREGLO EQU 8
-PACKED_NODO_OFFSET_LONGITUD EQU 8
-PACKED_NODO_SIZE EQU 8
-LISTA_OFFSET_HEAD EQU 8
+PACKED_NODO_OFFSET_ARREGLO EQU 9
+PACKED_NODO_OFFSET_LONGITUD EQU 17
+; -------
+PACKED_NODO_SIZE EQU 21
+
+
+LISTA_OFFSET_HEAD EQU 0
+; -------
 LISTA_SIZE EQU 8
-PACKED_LISTA_OFFSET_HEAD EQU 8
+
+PACKED_LISTA_OFFSET_HEAD EQU 0
+; -------
 PACKED_LISTA_SIZE EQU 8
 
 ;########### SECCION DE DATOS
@@ -29,12 +39,68 @@ global cantidad_total_de_elementos_packed
 
 ;########### DEFINICION DE FUNCIONES
 ;extern uint32_t cantidad_total_de_elementos(lista_t* lista);
-;registros: lista[?]
 cantidad_total_de_elementos:
-	ret
+;registros: 
+	; rdi = *lista
+	
+    ; === PRÓLOGO ===
+    push rbp
+    mov rbp, rsp
+	
+	xor eax, eax						; eax = cantidad = 0 
+
+	mov r8, qword[rdi + LISTA_OFFSET_HEAD]		; r8 = lista->head;
+
+.loop:
+    test r8, r8          ; condición de corte
+    jz .fin
+
+	mov r9d, dword[r8 + NODO_OFFSET_LONGITUD]
+
+	add eax, r9d
+
+.siguiente:
+    mov r8, qword[r8 + NODO_OFFSET_NEXT]
+    jmp .loop
+
+.fin:
+
+	; call cantidad_total_de_elementosC
+
+    ; === EPÍLOGO ===
+    pop rbp
+    ret
 
 ;extern uint32_t cantidad_total_de_elementos_packed(packed_lista_t* lista);
-;registros: lista[?]
 cantidad_total_de_elementos_packed:
-	ret
+;registros: 
+	; rdi = *lista
+	
+    ; === PRÓLOGO ===
+    push rbp
+    mov rbp, rsp
+	
+	xor eax, eax						; eax = cantidad = 0 
+
+	mov r8, qword[rdi + PACKED_LISTA_OFFSET_HEAD]		; r8 = lista->head;
+
+.loop:
+    test r8, r8          ; condición de corte
+    jz .fin
+
+	mov r9d, dword[r8 + PACKED_NODO_OFFSET_LONGITUD]
+
+	add eax, r9d
+
+.siguiente:
+    mov r8, qword[r8 + PACKED_NODO_OFFSET_NEXT]
+    jmp .loop
+
+.fin:
+
+	; call cantidad_total_de_elementos_packedC
+
+    ; === EPÍLOGO ===
+    pop rbp
+    ret
 
--- Guía de ASM 2c2026/src/3 - Alineación y Estructuras/Estructuras.c
+++ Guía de ASM 2c2026/src/3 - Alineación y Estructuras/Estructuras.c
@@ -1,3 +1,36 @@
 #include "Estructuras.h"
 
 /* Pueden programar alguna rutina auxiliar acá */
+
+uint32_t cantidad_total_de_elementosC(lista_t* lista){
+
+    uint32_t cantidad = 0;
+
+    nodo_t *actual = lista->head;
+
+    while (actual){
+        
+        cantidad += actual->longitud; 
+
+        actual = actual->next;
+    }
+
+    return cantidad;
+}
+
+
+uint32_t cantidad_total_de_elementos_packedC(packed_lista_t* lista){
+
+    uint32_t cantidad = 0;
+
+    packed_nodo_t *actual = lista->head;
+
+    while (actual){
+        
+        cantidad += actual->longitud; 
+
+        actual = actual->next;
+    }
+    
+    return cantidad;
+}
