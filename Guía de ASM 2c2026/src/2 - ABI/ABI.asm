ab6ce695b5b19a5ee4dc1850c73a98e4ce9f75c7
FelipePoliszuk <felipepoliszuk@gmail.com>
2026-09-06 22:54:40 -0300

    Apartado ABI terminado

--- Guía de ASM 2c2026/src/2 - ABI/ABI.asm
+++ Guía de ASM 2c2026/src/2 - ABI/ABI.asm
@@ -11,6 +11,7 @@ section .text
 global alternate_sum_4
 global alternate_sum_4_using_c
 global alternate_sum_4_using_c_alternative
+
 global alternate_sum_8
 global product_2_f
 global product_9_f
@@ -97,20 +98,71 @@ alternate_sum_4_using_c_alternative:
 
 
 ; uint32_t alternate_sum_8(uint32_t x1, uint32_t x2, uint32_t x3, uint32_t x4, uint32_t x5, uint32_t x6, uint32_t x7, uint32_t x8);
-; registros y pila: x1[?], x2[?], x3[?], x4[?], x5[?], x6[?], x7[?], x8[?]
 alternate_sum_8:
-	;prologo
+; registros:
+	; edi = x1
+	; esi = x2
+	; edx = x3
+	; ecx = x4
+	; r8d = x5
+	; r9d = x6
 
-	; COMPLETAR
+  ; [rbp + 16] = x7
+  ; [rbp + 24] = x8
+    
+    ; === PRÓLOGO ===
+    push rbp
+    mov rbp, rsp
 
-	;epilogo
-	ret
+    sub edi, esi
+    add edi, edx
+    sub edi, ecx
+    add edi, r8d
+    sub edi, r9d
 
+    add edi, dword[rbp + 16]
+    sub edi, dword[rbp + 24]
+
+    mov eax, edi
+
+    ; === EPÍLOGO ===
+    pop rbp
+    ret
+
+
+
+; Mapa conceptual de la Pila respecto a RBP
+
+;     [rbp + 24] -> Parámetro 8 (Puesto por el caller)
+
+;     [rbp + 16] -> Parámetro 7 (Puesto por el caller)
+
+;     [rbp + 8]  -> Dirección de retorno (Puesta automáticamente por el call)
+
+;     [rbp + 0]  -> El rbp original (Puesto por tu push rbp)
+
+;     [rbp - 8]  -> Tu primera variable local (o registro preservado)
+
+;     [rbp - 16] -> Tu segunda variable local
+
+;     [rbp - 24] -> Tu tercera variable local
 
 ; SUGERENCIA: investigar uso de instrucciones para convertir enteros a floats y viceversa
-;void product_2_f(uint32_t * destination, uint32_t x1, float f1);
-;registros: destination[?], x1[?], f1[?]
+;void product_2_f(uint32_t* destination, uint32_t x1, float f1);
 product_2_f:
+; registros:
+  ; rdi = *destination
+  ; esi  = x1
+  ; xmm0 = f1
+
+  cvtsi2ss xmm1, esi 
+  
+  mulss xmm0, xmm1
+
+  cvttss2si eax, xmm0 
+
+  mov dword[rdi], eax 
+
 	ret
 
 
@@ -118,24 +170,78 @@ product_2_f:
 ;, uint32_t x1, float f1, uint32_t x2, float f2, uint32_t x3, float f3, uint32_t x4, float f4
 ;, uint32_t x5, float f5, uint32_t x6, float f6, uint32_t x7, float f7, uint32_t x8, float f8
 ;, uint32_t x9, float f9);
-;registros y pila: destination[rdi], x1[?], f1[?], x2[?], f2[?], x3[?], f3[?], x4[?], f4[?]
-;	, x5[?], f5[?], x6[?], f6[?], x7[?], f7[?], x8[?], f8[?],
-;	, x9[?], f9[?]
+global product_9_f
 product_9_f:
-	;prologo
-	push rbp
-	mov rbp, rsp
+    ; === PRÓLOGO ===
+    push rbp
+    mov rbp, rsp
 
-	;convertimos los flotantes de cada registro xmm en doubles
-	; COMPLETAR
+    ; rdi = destination
+    ; esi = x1, edx = x2, ecx = x3, r8d = x4, r9d = x5
+    ; xmm0 = f1, xmm1 = f2, xmm2 = f3, xmm3 = f4, xmm4 = f5, xmm5 = f6, xmm6 = f7, xmm7 = f8
+    ; Pila: [rbp+16] = x6, [rbp+24] = x7, [rbp+32] = x8, [rbp+40] = x9, [rbp+48] = f9
 
-	;multiplicamos los doubles en xmm0 <- xmm0 * xmm1, xmmo * xmm2 , ...
-	; COMPLETAR
+    ; 1. Convertir los flotantes de los registros a doubles
+    cvtss2sd xmm0, xmm0
+    cvtss2sd xmm1, xmm1
+    cvtss2sd xmm2, xmm2
+    cvtss2sd xmm3, xmm3
+    cvtss2sd xmm4, xmm4
+    cvtss2sd xmm5, xmm5
+    cvtss2sd xmm6, xmm6
+    cvtss2sd xmm7, xmm7
 
-	; convertimos los enteros en doubles y los multiplicamos por xmm0.
-	; COMPLETAR
+    ; 2. Multiplicar todos los doubles presentes en registros
+    mulsd xmm0, xmm1
+    mulsd xmm0, xmm2
+    mulsd xmm0, xmm3
+    mulsd xmm0, xmm4
+    mulsd xmm0, xmm5
+    mulsd xmm0, xmm6
+    mulsd xmm0, xmm7
 
-	; epilogo
-	pop rbp
-	ret
+    ; 3. Extraer f9 de la pila, convertirlo y multiplicarlo
+    ; Usamos xmm1 temporalmente como auxiliar
+    cvtss2sd xmm1, dword [rbp + 48]
+    mulsd xmm0, xmm1
+
+    ; 4. Convertir enteros de registros a doubles y multiplicar
+    cvtsi2sd xmm1, rsi
+    mulsd xmm0, xmm1
+
+    cvtsi2sd xmm1, rdx
+    mulsd xmm0, xmm1
+
+    cvtsi2sd xmm1, rcx
+    mulsd xmm0, xmm1
+
+    cvtsi2sd xmm1, r8
+    mulsd xmm0, xmm1
+
+    cvtsi2sd xmm1, r9
+    mulsd xmm0, xmm1
+
+    ; 5. Extraer enteros de la pila, convertirlos y multiplicar
+    mov eax, dword [rbp + 16]   ; x6
+    cvtsi2sd xmm1, rax
+    mulsd xmm0, xmm1
+
+    mov eax, dword [rbp + 24]   ; x7
+    cvtsi2sd xmm1, rax
+    mulsd xmm0, xmm1
+
+    mov eax, dword [rbp + 32]   ; x8
+    cvtsi2sd xmm1, rax
+    mulsd xmm0, xmm1
+
+    mov eax, dword [rbp + 40]   ; x9
+    cvtsi2sd xmm1, rax
+    mulsd xmm0, xmm1
+
+    ; 6. Guardar el resultado final de 64 bits (double) en el puntero de memoria
+    movsd qword [rdi], xmm0
+
+    ; === EPÍLOGO ===
+    pop rbp
+    ret
 
